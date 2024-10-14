359b3c3b54e0fc2697498452a98a2a3eb812beab
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-15 00:44:22 +0200

    Added Response record

---
+++ src/ShopManagementService/ShopManagementService.Application/Common/Responses/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopManagementService.Application.Common.Responses;
+
+public record Response<T>
+{
+    public T Data { get; init; }
+    public bool Success { get; init; }
+    public string ErrorMessage { get; init; }
+}
