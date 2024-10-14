7d5966bf98de19760f7ea29b78f28c6a49ce8832
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-15 00:45:31 +0200

    Add DependencyInjection for Application project

---
+++ src/ShopManagementService/ShopManagementService.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ShopManagementService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediator();
+        
+        
+        return services;
+    }
+}
