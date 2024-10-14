e1d7f9617ff50a0b049663632f4dd2bee0e50c95
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-15 00:45:43 +0200

    Add product controller

---
+++ src/ShopManagementService/ShopManagementService.Api/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopManagementService.Api.Controllers;
+
+[ApiController]
+public class ProductController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
