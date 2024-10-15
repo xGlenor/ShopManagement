47af34748830f34dff4a9f3a1e8557a2c7bf7a14
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-15 20:45:22 +0200

    Delete Product command create

---
+++ src/ShopManagementService/ShopManagementService.Application/Modules/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Mediator;
+using ShopManagementService.Application.Common.Persistence.Repositories.Base;
+using ShopManagementService.Application.Common.Responses;
+
+namespace ShopManagementService.Application.Modules.Products.Commands;
+
+public record DeleteProductCommand(string Id) : IRequest<Response<bool>>;
+
+class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Response<bool>>
+{
+    private readonly IRepository<Product> _productRepository;
+
+    public DeleteProductCommandHandler(IRepository<Product> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async ValueTask<Response<bool>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var deletedResult = await _productRepository.DeleteAsync(request.Id, cancellationToken);
+
+        return deletedResult
+            ? new Response<bool> { Success = true }
+            : new Response<bool> { Success = false };
+    }
+}
