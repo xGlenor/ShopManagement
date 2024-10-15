ec28b2b1e6382d36c36c76839ca9fb53797a32c3
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-15 20:43:54 +0200

    Create Update product command

---
+++ src/ShopManagementService/ShopManagementService.Application/Modules/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Mapster;
+using MapsterMapper;
+using Mediator;
+using ShopManagementService.Application.Common.Persistence.Repositories.Base;
+using ShopManagementService.Application.Common.Responses;
+
+namespace ShopManagementService.Application.Modules.Products.Commands;
+
+public record UpdateProductCommand(
+    string Id,
+    string Name,
+    string Description,
+    decimal Price,
+    int StockQuantity,
+    bool IsAvailable
+) : IRequest<Response<bool>>;
+
+class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Response<bool>>
+{
+    private readonly IRepository<Product> _productRepository;
+
+    public UpdateProductCommandHandler(IRepository<Product> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async ValueTask<Response<bool>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        var productToUpdate = request.Adapt<Product>();
+        
+        var updateResult = await _productRepository.UpdateAsync(productToUpdate, cancellationToken);
+        // Todo -> Zapytać o to data, czy tak powinno być
+        return updateResult
+            ? new Response<bool> { Success = true, Data = true}
+            : new Response<bool> { Success = false, Data = false };
+    }
+}
