d396993985249ac1403e57b0f76784c1fad9cd50
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-17 22:35:13 +0200

    Parameter names changed

--- src/ShopManagementService/ShopManagementService.Application/Common/Persistence/Repositories/Base/IRepository.cs
+++ src/ShopManagementService/ShopManagementService.Application/Common/Persistence/Repositories/Base/IRepository.cs
@@ -5,8 +5,8 @@ namespace ShopManagementService.Application.Common.Persistence.Repositories.Base
 public interface IRepository<T> where T : class, IEntity
 {
     Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<T> GetByIdAsync(string id, CancellationToken cancellationToken = default);
+    Task<T> GetByIdAsync(string entityId, CancellationToken cancellationToken = default);
     Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
     Task<bool> UpdateAsync(T entity, CancellationToken cancellationToken = default);
-    Task<bool> DeleteAsync(string entity, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(string entityId, CancellationToken cancellationToken = default);
 }
