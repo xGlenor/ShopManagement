d660beb359ad1a5e5d4376b44a144ebbe2e5b206
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-24 23:09:11 +0200

    Created LoginUser Command

---
+++ src/ShopManagementService/ShopManagementService.Application/Modules/Users/Commands/LoginUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using Mediator;
+using ShopManagementService.Application.Common.Interfaces.Auth;
+using ShopManagementService.Application.Common.Responses;
+
+namespace ShopManagementService.Application.Modules.Users.Commands;
+
+public record LoginUserCommand(string Email, string Password) : IRequest<Response<string>>;
+
+class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, Response<string>>
+{
+    private readonly IUserRepository _repository;
+
+    public LoginUserCommandHandler(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+
+    public ValueTask<Response<string>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
