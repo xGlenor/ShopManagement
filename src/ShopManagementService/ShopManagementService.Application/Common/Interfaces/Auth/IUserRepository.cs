56bb53caeb93068f34f4623fb211c60302067cdb
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-24 23:08:49 +0200

    Created user interface and its implementation

---
+++ src/ShopManagementService/ShopManagementService.Application/Common/Interfaces/Auth/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using ShopManagementService.Application.Common.Dtos;
+using ShopManagementService.Application.Common.Responses;
+
+namespace ShopManagementService.Application.Common.Interfaces.Auth;
+
+public interface IUserRepository
+{
+    Task<ApplicationUser> RegisterUserAsync(ApplicationUser registerUser, CancellationToken cancellationToken = default);
+    
+    Task<ApplicationUser> FindUserByEmail(string Email, CancellationToken cancellationToken = default);
+    
+}
---
+++ src/ShopManagementService/ShopManagementService.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using MongoDB.Driver;
+using ShopManagementService.Application.Common.Dtos;
+using ShopManagementService.Application.Common.Interfaces.Auth;
+
+namespace ShopManagementService.Infrastructure.Persistence.Repositories;
+
+public class UserRepository: IUserRepository
+{
+    private readonly IMongoCollection<ApplicationUser> _users;
+
+    public UserRepository(IMongoClient client)
+    {
+        IMongoDatabase database = client.GetDatabase("ShopManagementDB");
+        _users = database.GetCollection<ApplicationUser>("users");
+    }
+
+    public async Task<ApplicationUser> RegisterUserAsync(ApplicationUser registerUser, CancellationToken cancellationToken = default)
+    {
+        await _users.InsertOneAsync(registerUser, cancellationToken: cancellationToken);
+        return registerUser;
+    }
+
+    public async Task<ApplicationUser> FindUserByEmail(string Email, CancellationToken cancellationToken = default)
+        => await _users.Find(p => p.Email == Email).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+    
+}
