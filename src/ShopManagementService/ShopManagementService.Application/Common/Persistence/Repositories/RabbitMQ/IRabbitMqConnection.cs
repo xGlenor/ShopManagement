96d520d87b03bc05a02fb18988de03226df3dece
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-24 23:10:29 +0200

    Created RabbitMQ connection interface and its implementation

---
+++ src/ShopManagementService/ShopManagementService.Application/Common/Persistence/Repositories/RabbitMQ/IRabbitMqConnection.cs
@@ -0,0 +1,8 @@
+﻿using RabbitMQ.Client;
+
+namespace ShopManagementService.Application.Common.Persistence.Repositories.RabbitMQ;
+
+public interface IRabbitMqConnection
+{
+    IConnection Connection { get; }
+}
---
+++ src/ShopManagementService/ShopManagementService.Application/Common/RabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using ShopManagementService.Application.Common.Persistence.Repositories.RabbitMQ;
+
+namespace ShopManagementService.Application.Common.RabbitMQ;
+
+public class RabbitMQConnection : IRabbitMqConnection, IDisposable
+{
+    private IConnection? _connection;
+
+    public IConnection Connection => _connection;
+
+    public RabbitMQConnection()
+    {
+        InitializeConnection();
+    }
+
+    private async void InitializeConnection()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+
+        _connection = await factory.CreateConnectionAsync();
+    }
+
+    public void Dispose()
+    {
+        _connection?.Dispose();
+    }
+}
